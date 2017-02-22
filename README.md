fc355fc90e406b7896842ba567119bbc255892e2
Artem Chernyaev <askanio234@gmail.com>
2017-02-23 01:36:40 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+Программа анализирует частоту употребления слов в тексте и выводит в консоль
+10 самых часто встречаемых слов
+
+Example usage:
+python lang_frequency.py
+Введите путь до файла: E:\Users\User\projects\5_lang_frequency\text.txt
+('the', 109)
+('of', 69)
+('to', 64)
+('that', 40)
+('in', 28)
+('was', 27)
+('their', 20)
+('had', 19)
+('and', 17)
+('be', 17)
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,36 @@
+import os
+import itertools
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, "r", encoding='utf-8') as file_handler:
+        return file_handler.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    word_list = text.split()
+    unique_word_list = []
+    for word in word_list:
+        if (word not in unique_word_list) and word.isalnum():
+            unique_word_list.append(word)
+    word_count = []
+    for word in unique_word_list:
+        word_count.append(word_list.count(word))
+    freq_pairs = zip(unique_word_list, word_count)
+    sorted_freq_pairs = sorted(
+                        freq_pairs, key=lambda freq_pairs: freq_pairs[1],
+                        reverse=True)
+    return sorted_freq_pairs
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input("Введите путь до файла: ")
+    if load_data(filepath) is not None:
+        sorted_freq_pairs = get_most_frequent_words((load_data(filepath)))
+        top_ten_words = itertools.islice(sorted_freq_pairs, 10)
+        for word in top_ten_words:
+            print(word)
+    else:
+        print("Некорректный путь до файла")
